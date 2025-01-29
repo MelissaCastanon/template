4592274c32b17b58fca82acb8733fcd39b68a254
MelissaCastanon <edsamelissa.casta21@gmail.com>
2025-01-28 21:05:44 -0600

    Actualización de todas las paqueterias y librerias

--- frontend/src/layouts/dashboard/components/calendario/index.js
+++ frontend/src/layouts/dashboard/components/calendario/index.js
@@ -1,24 +1,12 @@
 import * as React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-export default function StaticDatePickerDemo() {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'));
-
+export default function BasicDateCalendar() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker
-        displayStaticWrapperAs="desktop"
-        openTo="year"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
+      <DateCalendar />
     </LocalizationProvider>
   );
 }
